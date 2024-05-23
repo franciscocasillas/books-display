48cd7169f29668a28ea208cc72a78591264387ad
Francisco J. Casillas <chiccasillas@gmail.com>
2024-05-23 15:15:40 -0600

    Fix Books component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
-import { books } from "./data/books.js";
 import Books from "./components/Books.jsx";
 
 function App() {
 	return (
 		<div>
-			{books.map((book) => (
-				<Books key={book.title} {...book} />
-			))}
+			<h1>My Book Collection</h1>
+			<Books />
 		</div>
 	);
 }
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,19 +1,18 @@
 import "../styles/Books.css";
-import PropTypes from "prop-types";
 import { books } from "../data/books.js";
 
-function Books({ title, author, rating }) {
-	<div>
-		<h1>{title}</h1>
-		<h2>{author}</h2>
-		<p>{rating}</p>
-	</div>;
+function Books() {
+	return (
+		<ul>
+			{books.map((book) => (
+				<li key={book.id}>
+					<h1>{book.title}</h1>
+					<h2>By {book.author}</h2>
+					<p>Rating: {book.rating}</p>
+				</li>
+			))}
+		</ul>
+	);
 }
-
-Books.propTypes = {
-	title: PropTypes.string.isRequired,
-	author: PropTypes.string.isRequired,
-	rating: PropTypes.number.isRequired,
-};
 
 export default Books;
